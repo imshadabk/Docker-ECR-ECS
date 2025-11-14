b381feb3180640d1817ec1a5f3dc51976ad5b801
shadab05 <shadabiskylar@gmail.com>
2025-11-14 12:24:22 +0000

    added changes

---
+++ iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "ecs_task_execution" {
+name = "react-app-ecs-exec-role"
+assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role.json
+}
+
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role" {
+statement {
+actions = ["sts:AssumeRole"]
+principals {
+type = "Service"
+identifiers = ["ecs-tasks.amazonaws.com"]
+}
+}
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+role = aws_iam_role.ecs_task_execution.name
+policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
